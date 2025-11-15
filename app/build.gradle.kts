a3c26c55bba720750bc4bc5813ee837445c8206d
ar-sarkar-77 <anondosarkarar77@gmail.com>
2025-11-15 20:30:12 +0600

    logo update,ssp sdp implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // ssp & sdp
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
+
 }
