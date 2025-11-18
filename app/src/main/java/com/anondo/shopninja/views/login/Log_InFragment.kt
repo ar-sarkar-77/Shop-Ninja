7e851d5b320610258eded50d34b4678db43a9ca4
ar-sarkar-77 <anondosarkarar77@gmail.com>
2025-11-18 13:11:58 +0600

    Auth with BaseFragment , Password UI update

--- app/src/main/java/com/anondo/shopninja/views/login/Log_InFragment.kt
+++ app/src/main/java/com/anondo/shopninja/views/login/Log_InFragment.kt
@@ -1,37 +1,22 @@
 package com.anondo.shopninja.views.login
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.anondo.shopninja.R
+import com.anondo.shopninja.base.BaseFragment
 import com.anondo.shopninja.core.DataState
 import com.anondo.shopninja.data.models.UserLogIn
-import com.anondo.shopninja.isEmptyfun
 import com.anondo.shopninja.databinding.FragmentLogInBinding
+import com.anondo.shopninja.isEmptyfun
 
 
-class Log_InFragment : Fragment() {
+class Log_InFragment : BaseFragment<FragmentLogInBinding>(FragmentLogInBinding::inflate) {
 
-    lateinit var binding: FragmentLogInBinding
+
     private val viewModel : LogInViewModel by viewModels ()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        binding = FragmentLogInBinding.inflate(inflater , container , false)
-        setLlistener()
-        loginResponce()
-        return binding.root
-    }
-
-    private fun setLlistener(){
+    override fun setListener(){
 
         with(binding){
 
@@ -55,18 +40,21 @@ class Log_InFragment : Fragment() {
         }
     }
 
-    private fun loginResponce() {
+    //OOAD -> Object Oriented Analytics Design (Code Design)
+    override fun observer() {
 
         viewModel.logIn_Responce.observe(viewLifecycleOwner){
 
             when(it){
                 is DataState.Error -> {
+                    loading_progress_dialog.dismiss()
                     Toast.makeText(context , it.message.toString(), Toast.LENGTH_SHORT).show()
                 }
                 is DataState.Loading -> {
-
+                    loading_progress_dialog.show()
                 }
                 is DataState.Success -> {
+                    loading_progress_dialog.dismiss()
                     Toast.makeText(context , "Log in success!!", Toast.LENGTH_SHORT).show()
 
                     findNavController().navigate(R.id.dashBoardFragment)
--- app/src/main/java/com/anondo/shopninja/views/register/RegisterFragment.kt
+++ app/src/main/java/com/anondo/shopninja/views/register/RegisterFragment.kt
@@ -1,42 +1,21 @@
 package com.anondo.shopninja.views.register
 
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.anondo.shopninja.R
+import com.anondo.shopninja.base.BaseFragment
 import com.anondo.shopninja.core.DataState
 import com.anondo.shopninja.data.models.UserRegistation
 import com.anondo.shopninja.databinding.FragmentRegisterBinding
 import com.anondo.shopninja.isEmptyfun
 
-class RegisterFragment : Fragment() {
-
-    lateinit var binding: FragmentRegisterBinding
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>(FragmentRegisterBinding::inflate) {
     private val viewModel : RegisterViewModel by viewModels ()
     private var userType : String = ""
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        binding = FragmentRegisterBinding.inflate(inflater , container , false)
-
-        setRListener()
-
-        registationResponce()
-
-        return binding.root
-    }
-
-
-
-    private fun setRListener(){
+    override fun setListener(){
 
 
 
@@ -90,19 +69,22 @@ class RegisterFragment : Fragment() {
 
     }
 
-    private fun registationResponce() {
+    //OOAD -> Object Oriented Analytics Design (Code Design)
+    override fun observer() {
 
         viewModel.registation_Responce.observe(viewLifecycleOwner){
 
             when(it){
                 is DataState.Error -> {
+                    loading_progress_dialog.dismiss()
                     Toast.makeText(context , it.message.toString(), Toast.LENGTH_SHORT).show()
                 }
                 is DataState.Loading -> {
-                    Toast.makeText(context , "Loading", Toast.LENGTH_SHORT).show()
+                    loading_progress_dialog.show()
                 }
                 is DataState.Success -> {
-                    Toast.makeText(context , it.data.toString(), Toast.LENGTH_SHORT).show()
+                    loading_progress_dialog.dismiss()
+                    Toast.makeText(context , "Registation Success!!", Toast.LENGTH_SHORT).show()
 
                     findNavController().navigate(R.id.dashBoardFragment)
                 }
